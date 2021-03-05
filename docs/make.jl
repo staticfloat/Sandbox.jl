7d9e713117e0811eb98a251f4d6f0ab95b323b15
Mosè Giordano <giordano@users.noreply.github.com>
2021-03-05 00:07:16 +0000

    Build documentation with Documenter (#12)

---
+++ docs/make.jl
@@ -0,0 +1,11 @@
+using Documenter, Sandbox
+
+makedocs(
+    modules = [Sandbox],
+    sitename = "Sandbox.jl",
+)
+
+deploydocs(
+    repo = "github.com/staticfloat/Sandbo.jl.git",
+    push_preview = true,
+)
